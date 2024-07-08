cc98b549981d24953c1458827b6ff0d64c83c2c2
MMMaxi12 <maxiliendro1403@gmail.com>
2024-07-07 23:43:09 -0300

    ejercicio4 terminado sin funcionalidades adicionales

--- ejercicio4/memory.js
+++ ejercicio4/memory.js
@@ -31,6 +31,25 @@ class Card {
         const cardElement = this.element.querySelector(".card");
         cardElement.classList.remove("flipped");
     }
+
+    toggleFlip() {
+
+        this.isFlipped = !this.isFlipped;
+
+        if (this.isFlipped) {
+            this.#flip();
+        } else {
+            this.#unflip();
+        }
+
+    }
+
+    matches(otherCard) {
+
+        return this.name === otherCard.name;
+
+    }
+
 }
 
 class Board {
@@ -74,6 +93,33 @@ class Board {
             this.onCardClick(card);
         }
     }
+
+    shuffleCards() {
+
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+        
+    }
+
+    flipDownAllCards() {
+
+        for(let i = 0; i<this.cards.length ; i++){
+            if(this.cards[i].isFlipped){
+                this.cards[i].toggleFlip();
+            }
+        }
+
+    }
+
+    reset() {
+
+        this.shuffleCards();
+        this.flipDownAllCards();
+        this.render();
+
+    }
 }
 
 class MemoryGame {
@@ -102,6 +148,29 @@ class MemoryGame {
             }
         }
     }
+
+    checkForMatch() {
+
+        const [card1, card2] = this.flippedCards;
+        if (card1.matches(card2)) {
+            this.matchedCards.push(card1, card2);
+        } else {
+            card1.toggleFlip();
+            card2.toggleFlip();
+        }
+        this.flippedCards = [];
+
+    }
+
+    resetGame() {
+
+        this.flippedCards = [];
+        this.matchedCards = [];
+        this.board.reset();
+
+    }
+
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
